2a71bf33056e76459b8f0cc24f7416e4aa9e6148
Hakan ÖZER <hakanozer02@gmail.com>
2026-09-23 09:07:13 +0300

    Project

--- RestApi/obj/Debug/net9.0/RestApi.AssemblyInfo.cs
+++ RestApi/obj/Debug/net9.0/RestApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b20337790a1f7df23c19655ef98d93bd62d0df04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aeb7ae5fe2e497f147fbf510fb3a2c0d5073b87")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
